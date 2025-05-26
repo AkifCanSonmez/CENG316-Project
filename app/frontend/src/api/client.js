b288eacc906a32514a6362b736776329907e9de0
root <root@canputer.>
2025-05-26 03:10:05 +0300

    updated

--- app/frontend/src/api/client.js
+++ app/frontend/src/api/client.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Backend API temel URL'i
 // Gerekirse .env veya hardcode olarak değiştirin
 const client = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://ceng316-project-production-85ee.up.railway.app',
 });
 
 export default client;
